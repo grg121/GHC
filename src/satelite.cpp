05e81b0415574617372b99d73aa3771c56c95d75
juanka1995 <jcarlosruiz95@correo.ugr.es>
2017-02-18 14:00:18 +0100

    nombre funciones adjust-lon/lat

--- src/satelite.cpp
+++ src/satelite.cpp
@@ -1,31 +1,23 @@
 #include "satelite.h"
 
-inline long int adjust_lat(long int arcs, bool & cambia_velocidad){
-  long int resultado;
-  if (arcs >= ((-90)*3600) && arcs <= (90*3600)) {
-    resultado = arcs;
-    cambia_velocidad = false;
-  }
-  else if(arcs > (90*3600)){
-    resultado = (180*3600) - arcs;
-    cambia_velocidad = true;
+inline bool adjust_lat(int &arcs){
+  bool desborda;
+  if (arcs >= ((-90)*3600) && arcs <= (90*3600) ) {
+    desborda = false;
   }
   else if(arcs < ((-90)*3600)){
-    resultado = ((-180)*3600) - arcs;
-    cambia_velocidad = true;
+    arcs = (-180) - arcs;
+    desborda = true;
   }
-  return resultado;
+  else if(arcs > (90*3600)){
+    arcs = 180 - arcs;
+    desborda = true;
+  }
+  return desborda;
 }
 
-inline long int adjust_lon(long int arcs, bool & cambia_velocidad){
-  long int resultado;
-  if (arcs >= ((-90)*3600) && arcs <= (90*3600)) {
-    resultado = arcs;
-    cambia_velocidad = false;
+inline int adjust_lon(int arcs, bool &desb){
+  if (arcs <= 647999 && ) {
+    /* code */
   }
-  else {
-    resultado = ((-180)*3600) + arcs;
-    cambia_velocidad = true;
-  }
-  return resultado;
 }
