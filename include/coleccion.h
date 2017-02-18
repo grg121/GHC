df351d809e0428dfe1e942c8685f3a4d32e5c462
juanka1995 <jcarlosruiz95@correo.ugr.es>
2017-02-18 10:43:02 +0100

    Directorios creados

---
+++ include/coleccion.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+class Coleccion{
+  private:
+    const long int puntuacion;
+    list<int> rangos;
+    list<Punto> &puntos;
+    int tam
+  public:
+
+};
