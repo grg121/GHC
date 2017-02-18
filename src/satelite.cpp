0407955913a37a44a748310880b6d2045edbee4d
juanka1995 <jcarlosruiz95@correo.ugr.es>
2017-02-18 12:36:05 +0100

    fixed adjust_last and adjust_lon

--- src/satelite.cpp
+++ src/satelite.cpp
@@ -2,17 +2,16 @@
 
 inline long int adjust_lat(long int arcs, bool & cambia_velocidad){
   long int resultado;
-  arcs *= 3600;
-  if (arcs >= (-90) && arcs <= 90) {
+  if (arcs >= ((-90)*3600) && arcs <= (90*3600)) {
     resultado = arcs;
     cambia_velocidad = false;
   }
-  else if(arcs > 90){
-    resultado = 180 - arcs;
+  else if(arcs > (90*3600)){
+    resultado = (180*3600) - arcs;
     cambia_velocidad = true;
   }
-  else if(arcs < (-90)){
-    resultado = (-180) - arcs;
+  else if(arcs < ((-90)*3600)){
+    resultado = ((-180)*3600) - arcs;
     cambia_velocidad = true;
   }
   return resultado;
@@ -20,13 +19,12 @@ inline long int adjust_lat(long int arcs, bool & cambia_velocidad){
 
 inline long int adjust_lon(long int arcs, bool & cambia_velocidad){
   long int resultado;
-  arcs *= 3600;
-  if (arcs >= (-90) && arcs <= 90) {
+  if (arcs >= ((-90)*3600) && arcs <= (90*3600)) {
     resultado = arcs;
     cambia_velocidad = false;
   }
   else {
-    resultado = (-180) + arcs;
+    resultado = ((-180)*3600) + arcs;
     cambia_velocidad = true;
   }
   return resultado;
